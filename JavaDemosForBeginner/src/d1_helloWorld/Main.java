b6f6694975a7122f3092fb4b7349bfc910919790
BusraYaydemir <147690122+BusraYaydemir@users.noreply.github.com>
2024-01-22 01:18:30 +0300

    Demo 1

--- JavaDemosForBeginner/src/d1_helloWorld/Main.java
+++ JavaDemosForBeginner/src/d1_helloWorld/Main.java
@@ -1,10 +1,11 @@
 package d1_helloWorld;
 
 public class Main {
-
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		System.out.println("Hello World!");
+		
 	}
 
 }
