240c9fb00a6d47e4ceef6063f864e27546ecd82d
BusraYaydemir <147690122+BusraYaydemir@users.noreply.github.com>
2024-01-22 01:20:55 +0300

    Demo 2

---
+++ JavaDemosForBeginner/src/d2_variables/Main.java
@@ -0,0 +1,28 @@
+package d2_variables;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		/*
+         * Java is a Case Sensitive language.
+         * Case Sensitive refers to the distinction between uppercase and lowercase letters. For example, in a case-sensitive system, "ABC" and "abc" would be considered different values.
+         */
+
+        /*
+         * Reusability :
+         * Code reusability is the capacity to repurpose pre-existing code when developing new software applications. Ideally, code reuse should be easy to implement, and any stable, functional code could freely be reused when building a new software application.
+         * Reduces memory usage.
+         */
+
+        int studentAmount = 12;
+
+        System.out.println("Student Amount: "+ studentAmount);
+
+        String message = "Student Amount: ";
+
+        System.out.println(message + studentAmount);
+        
+	}
+
+}
